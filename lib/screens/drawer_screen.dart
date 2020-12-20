59b4bdadfee50fe95afbab6862e39b1c96c65c7d
Youcef Messelem <messelemy@gmail.com>
2020-12-20 12:07:44 +0400

    Ended with the project by adding the components to the wrapper page

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../configuration.dart';
 
-
 class DrawerScreen extends StatefulWidget {
   @override
   _DrawerScreenState createState() => _DrawerScreenState();
@@ -11,7 +10,84 @@ class _DrawerScreenState extends State<DrawerScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top: 50, bottom: 40, left: 10),
       color: primaryGreen,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+              ),
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Youcef Messelem",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                  Text("Active Status",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ))
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: drawerItems
+                .map((element) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            element['icon'],
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          SizedBox(width: 10),
+                          Text(element['title'],
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20))
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
+          Row(
+            children: <Widget>[
+              Icon(Icons.settings, color: Colors.white),
+              SizedBox(
+                width: 10,
+              ),
+              Text("Settings",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20)),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 2,
+                height: 20,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text("Logout",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,9 +8,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  double xOffset=0;
+  double xOffset = 0;
   double yOffset = 0;
+  double curvedEdges = 0;
 
   double scaleFactor = 1;
 
@@ -19,11 +19,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      transform: Matrix4.translationValues(xOffset, yOffset, 0)..scale(scaleFactor),
+      transform: Matrix4.translationValues(xOffset, yOffset, 0)
+        ..scale(scaleFactor),
       duration: Duration(milliseconds: 250),
-      color: Colors.grey[200],
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(curvedEdges)),
       child: SingleChildScrollView(
-              child: Column(
+        child: Column(
           children: [
             SizedBox(height: 50),
             Container(
@@ -31,93 +34,89 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  isDrawerOpen ? 
-                  IconButton(icon: Icon(Icons.menu), onPressed: (){
-                    setState(() {
-                      xOffset = 0;
-                      yOffset = 0;
-                      scaleFactor = 1;
-                      isDrawerOpen = false;
-                    });
-                  }) :
-                  IconButton(icon: Icon(Icons.menu), onPressed: (){
-                    setState(() {
-                      xOffset = 250;
-                      yOffset = 150;
-                      scaleFactor = 0.5;
-                      isDrawerOpen = true;
-                    });
-                  }),
-                  Column(
-                    children: [
-                      Text("Location"),
-                      Row(
-                        children: [
-                          Icon(Icons.location_on, color: primaryGreen),
-                          Text("Dubai"),
-                        ],
-                      ),
-                    ]
-                  ),
-                  CircleAvatar(backgroundColor: Colors.blue,)
-
+                  isDrawerOpen
+                      ? IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 0;
+                              yOffset = 0;
+                              scaleFactor = 1;
+                              curvedEdges = 0;
+                              isDrawerOpen = false;
+                            });
+                          })
+                      : IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 300;
+                              yOffset = 100;
+                              scaleFactor = 0.8;
+                              curvedEdges = 40;
+                              isDrawerOpen = true;
+                            });
+                          }),
+                  Column(children: [
+                    Text("Location"),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, color: primaryGreen),
+                        Text("Dubai"),
+                      ],
+                    ),
+                  ]),
+                  CircleAvatar(
+                    backgroundColor: Colors.blue,
+                  )
                 ],
               ),
             ),
-        
             Container(
-              padding: EdgeInsets.symmetric(horizontal:20, vertical: 15),
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical:20),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white
-              ),
+                  borderRadius: BorderRadius.circular(20), color: Colors.white),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children:[
-                  Icon(Icons.search),
-                  Text("Search to adopt"),
-                  Icon(Icons.settings)
-                ]
-              ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.search),
+                    Text("Search to adopt"),
+                    Icon(Icons.settings)
+                  ]),
             ),
             Container(
               height: 120,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: categories.length,
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   return Container(
-                    child: Column(
-                      children:[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.only(left: 20),
-                          decoration: BoxDecoration(
+                    child: Column(children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
                             color: Colors.white,
                             boxShadow: shadowList,
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Image.asset(
-                            categories[index]["iconPath"],
-                            height: 50,
-                            width: 50,
-                            color: Colors.grey[700],
-                          ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Image.asset(
+                          categories[index]["iconPath"],
+                          height: 50,
+                          width: 50,
+                          color: Colors.grey[700],
                         ),
-                        Text(categories[index]["name"])
-                      ]
-                    ),
+                      ),
+                      Text(categories[index]["name"])
+                    ]),
                   );
                 },
-                ),
-
               ),
-         
-            
+            ),
             GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductScreen()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ProductScreen()));
               },
               child: Container(
                 height: 240,
@@ -128,20 +127,16 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Stack(
                         children: [
                           Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blueGrey[500],
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: shadowList
-                            ),
-                            margin: EdgeInsets.only(top: 40)
-                          ),
+                              decoration: BoxDecoration(
+                                  color: Colors.blueGrey[500],
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: shadowList),
+                              margin: EdgeInsets.only(top: 40)),
                           Align(
-                            alignment: Alignment(0.0, 0.0),
-                            child: Hero(
-                              tag: 1,
-                              child: Image.asset("images/pet-cat2.png")
-                              )
-                          )
+                              alignment: Alignment(0.0, 0.0),
+                              child: Hero(
+                                  tag: 1,
+                                  child: Image.asset("images/pet-cat2.png")))
                         ],
                       ),
                     ),
@@ -150,48 +145,44 @@ class _HomeScreenState extends State<HomeScreen> {
                         margin: EdgeInsets.only(top: 60, bottom: 20),
                         padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                         decoration: BoxDecoration(
-                          boxShadow: shadowList,
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(20))
-                        ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20))),
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children:[
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(
                                     "Carrot",
                                     style: TextStyle(
-                                      fontSize: 25.0,
-                                      fontWeight: FontWeight.bold
-                                    ),
-                                    ),
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                   Icon(Icons.face)
                                 ],
                               ),
-                           
-                            Text(
-                              "Arabian Mao", 
-                              style: TextStyle(
-                                fontSize:  15.0,
+                              Text(
+                                "Arabian Mao",
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                ),
                               ),
-                            ),
-                            Text("2 years old"),
-                            Container(
-                              child: Row(
-                                children: [
+                              Text("2 years old"),
+                              Container(
+                                child: Row(children: [
                                   Icon(
                                     Icons.location_on,
                                     color: Colors.blueGrey,
-                                    ),
+                                  ),
                                   Text("Distance: 2KM")
-                                ]
-                              ),
-                            )
-                          ]
-                        ),
+                                ]),
+                              )
+                            ]),
                       ),
                     )
                   ],
@@ -207,17 +198,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Stack(
                       children: [
                         Container(
-                          decoration: BoxDecoration(
-                            color: Colors.orange[100],
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: shadowList
-                          ),
-                          margin: EdgeInsets.only(top: 40)
-                        ),
+                            decoration: BoxDecoration(
+                              color: Colors.orange[100],
+                              borderRadius: BorderRadius.circular(20),
+                              //boxShadow: shadowList
+                            ),
+                            margin: EdgeInsets.only(top: 40)),
                         Align(
-                          alignment: Alignment(0.0, 0.0),
-                          child: Image.asset("images/pet-cat1.png")
-                        )
+                            alignment: Alignment(0.0, 0.0),
+                            child: Image.asset("images/pet-cat1.png"))
                       ],
                     ),
                   ),
@@ -226,48 +215,44 @@ class _HomeScreenState extends State<HomeScreen> {
                       margin: EdgeInsets.only(top: 60, bottom: 20),
                       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                       decoration: BoxDecoration(
-                        boxShadow: shadowList,
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(20))
-                      ),
+                          boxShadow: shadowList,
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              bottomRight: Radius.circular(20))),
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:[
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
                                   "Carrot",
                                   style: TextStyle(
-                                    fontSize: 25.0,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                  ),
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
                                 Icon(Icons.face)
                               ],
                             ),
-                         
-                          Text(
-                            "Arabian Mao", 
-                            style: TextStyle(
-                              fontSize:  15.0,
+                            Text(
+                              "Arabian Mao",
+                              style: TextStyle(
+                                fontSize: 15.0,
+                              ),
                             ),
-                          ),
-                          Text("2 years old"),
-                          Container(
-                            child: Row(
-                              children: [
+                            Text("2 years old"),
+                            Container(
+                              child: Row(children: [
                                 Icon(
                                   Icons.location_on,
                                   color: Colors.blueGrey,
-                                  ),
+                                ),
                                 Text("Distance: 2KM")
-                              ]
-                            ),
-                          )
-                        ]
-                      ),
+                              ]),
+                            )
+                          ]),
                     ),
                   )
                 ],
