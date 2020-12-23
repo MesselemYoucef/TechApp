38a68cf059499a9bbabbe9983613a6432430dea4
Youcef Messelem <messelemy@gmail.com>
2020-12-23 16:34:55 +0400

    add the gesture to container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,127 +18,140 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      transform: Matrix4.translationValues(xOffset, yOffset, 0)
-        ..scale(scaleFactor),
-      duration: Duration(milliseconds: 250),
-      decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(curvedEdges)),
-      child: Column(
-        children: [
-          SizedBox(height: 30),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                isDrawerOpen
-                    ? IconButton(
-                        icon: Icon(Icons.menu),
-                        onPressed: () {
-                          setState(() {
-                            xOffset = 0;
-                            yOffset = 0;
-                            scaleFactor = 1;
-                            curvedEdges = 0;
-                            isDrawerOpen = false;
-                          });
-                        })
-                    : IconButton(
-                        icon: Icon(Icons.menu),
-                        onPressed: () {
-                          setState(() {
-                            xOffset = 300;
-                            yOffset = 100;
-                            scaleFactor = 0.8;
-                            curvedEdges = 40;
-                            isDrawerOpen = true;
-                          });
-                        }),
-                Column(children: [
-                  Text("Location"),
-                  Row(
-                    children: [
-                      Icon(Icons.location_on, color: primaryGreen),
-                      Text("Dubai"),
-                    ],
-                  ),
-                ]),
-                Icon(
-                  Icons.mail,
-                  color: Colors.grey[800],
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20), color: Colors.white),
-            child: Row(
+    return GestureDetector(
+      onTap: () {
+        if (isDrawerOpen) {
+          setState(() {
+            xOffset = 0;
+            yOffset = 0;
+            scaleFactor = 1;
+            curvedEdges = 0;
+            isDrawerOpen = false;
+          });
+        }
+      },
+      child: AnimatedContainer(
+        transform: Matrix4.translationValues(xOffset, yOffset, 0)
+          ..scale(scaleFactor),
+        duration: Duration(milliseconds: 250),
+        decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(curvedEdges)),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.search),
-                  Text(
-                    "Search Product",
-                    style: TextStyle(color: Colors.grey, fontSize: 15),
-                  ),
-                  Icon(Icons.settings)
-                ]),
-          ),
-          Container(
-            height: 80,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Column(children: [
-                    Container(
-                      padding: EdgeInsets.all(8),
-                      margin: EdgeInsets.only(left: 20),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: shadowList,
-                          borderRadius: BorderRadius.circular(5)),
-                      child: Image.asset(
-                        categories[index]["iconPath"],
-                        height: 40,
-                        width: 40,
-                        color: Colors.grey[700],
-                        fit: BoxFit.contain,
-                      ),
+                  isDrawerOpen
+                      ? IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 0;
+                              yOffset = 0;
+                              scaleFactor = 1;
+                              curvedEdges = 0;
+                              isDrawerOpen = false;
+                            });
+                          })
+                      : IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 300;
+                              yOffset = 100;
+                              scaleFactor = 0.8;
+                              curvedEdges = 40;
+                              isDrawerOpen = true;
+                            });
+                          }),
+                  Column(children: [
+                    Text("Location"),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, color: primaryGreen),
+                        Text("Dubai"),
+                      ],
                     ),
-                    SizedBox(height: 5),
-                    Container(
-                        margin: EdgeInsets.only(left: 20),
-                        child: Text(
-                          categories[index]["name"],
-                          style: TextStyle(
-                              fontSize: 10, fontWeight: FontWeight.bold),
-                        ))
                   ]),
-                );
-              },
+                  Icon(
+                    Icons.mail,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: itemDetails.length,
-              itemBuilder: (context, index) {
-                return ItemTile(
-                  productName: itemDetails[index]['productName'],
-                  price: itemDetails[index]['price'],
-                  condition: itemDetails[index]['condition'],
-                  distance: itemDetails[index]['distance'],
-                  image: itemDetails[index]['image'],
-                );
-              },
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.white),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.search),
+                    Text(
+                      "Search Product",
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    Icon(Icons.settings)
+                  ]),
             ),
-          ),
-        ],
+            Container(
+              height: 80,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        margin: EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: shadowList,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Image.asset(
+                          categories[index]["iconPath"],
+                          height: 40,
+                          width: 40,
+                          color: Colors.grey[700],
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: Text(
+                            categories[index]["name"],
+                            style: TextStyle(
+                                fontSize: 10, fontWeight: FontWeight.bold),
+                          ))
+                    ]),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: itemDetails.length,
+                itemBuilder: (context, index) {
+                  return ItemTile(
+                    productName: itemDetails[index]['productName'],
+                    price: itemDetails[index]['price'],
+                    condition: itemDetails[index]['condition'],
+                    distance: itemDetails[index]['distance'],
+                    image: itemDetails[index]['image'],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/item_tile.dart
+++ lib/screens/item_tile.dart
@@ -16,68 +16,72 @@ class ItemTile extends StatelessWidget {
       this.price});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Container(
-                    decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product_screen');
+      },
+      child: Container(
+        height: 220,
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
                         color: itemBackground,
                         borderRadius: BorderRadius.only(
                             bottomLeft: Radius.circular(10),
                             bottomRight: Radius.circular(20),
                             topLeft: Radius.circular(20),
                             topRight: Radius.circular(10)),
-                        ),
-                    margin: EdgeInsets.only(top: 20)),
-                Align(
-                    alignment: Alignment(0.0, 0.0),
-                    child: Hero(tag: 1, child: Image.asset(image)))
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(top: 60, bottom: 20),
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                  )),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      productName,
-                      style: TextStyle(
-                          fontSize: 15.0, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      price,
-                      style: TextStyle(
-                        fontSize: 15.0,
                       ),
-                    ),
-                    Text("$condition old"),
-                    Container(
-                      child: Row(children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.blueGrey,
-                        ),
-                        Text("Distance: $distance")
-                      ]),
-                    )
-                  ]),
+                      margin: EdgeInsets.only(top: 20)),
+                  Align(
+                      alignment: Alignment(0.0, 0.0), child: Image.asset(image))
+                ],
+              ),
             ),
-          )
-        ],
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 60, bottom: 20),
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                    )),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        productName,
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        price,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Text("$condition old"),
+                      Container(
+                        child: Row(children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.blueGrey,
+                          ),
+                          Text("Distance: $distance")
+                        ]),
+                      )
+                    ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
