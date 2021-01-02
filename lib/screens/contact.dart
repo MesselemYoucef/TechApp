4dbc7818f7383185234202f8edfeb5940ce707ab
Youcef Messelem <messelemy@gmail.com>
2021-01-02 16:05:22 +0400

    added the image in contact us page

--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:petsApp/configuration.dart';
 
-
 class Contact extends StatefulWidget {
   @override
   _ContactState createState() => _ContactState();
@@ -15,23 +14,27 @@ class _ContactState extends State<Contact> {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Text(
-                  "Contact Us",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.fjallaOne(
-                    textStyle: TextStyle(
-                      color: itemBackground,
-                      fontSize: 50.0
-                    ),
-                  ),
+          child: Column(children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Contact Us",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.fjallaOne(
+                  textStyle: TextStyle(color: itemBackground, fontSize: 50.0),
                 ),
               ),
-            ]
-          ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              color: Colors.teal,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3,
+              child: Image.asset("images/contact-us.png"),
+            ),
+          ]),
         ),
       ),
     );
